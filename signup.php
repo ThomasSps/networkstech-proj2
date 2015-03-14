c7d2c1949ec71ecc7dae94dab677c902289ba528
Thomas Henry Spiteri <thomassps@gmail.com>
2015-03-14 22:23:56 +0200

    Bugfix 1.0

--- signup.php
+++ signup.php
@@ -4,7 +4,9 @@
 	$database = new DB_Provider();
 	$database -> connect();
 
-	$query = sprintf( "INSERT INTO 'user' ( 'uname', 'pass' ) VALUES ( '%s', '%s' )  ", mysql_real_escape_string( $_POST['uname'] ), mysql_real_escape_string( $_POST['pass'] ) );
+	$query = sprintf( "INSERT INTO 'user' ( '%s', '%s' )", mysql_real_escape_string( $_POST["uname"] ), mysql_real_escape_string( $_POST["pass"] ) );
+
+	mysql_query($database, $query);
 
 	$database -> close(); 
 ?>
