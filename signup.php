d40f7cf7a627a0d7f38a12fc09468405334a7dde
Thomas Henry Spiteri <thomassps@gmail.com>
2015-03-14 23:10:53 +0200

    Bugfix v4.0

--- signup.php
+++ signup.php
@@ -4,9 +4,21 @@
 	$database = new DB_Provider();
 	$database -> connect();
 
-	$query = sprintf( "INSERT INTO `user`(`uname`, `pass`) VALUES ( '%s', '%s')", mysql_real_escape_string( isset($_POST["uname"]) ), mysql_real_escape_string( isset($_POST["pass"]) ) );
 
-	mysql_db_query( $database, $query );
+	if( isset($_POST["uname"]) && $_POST["pass"] )
+	{
+		$query = sprintf( "INSERT INTO `user`(`uname`, `pass`) VALUES ( '%s', '%s')", mysql_real_escape_string( $_POST["uname"] ), mysql_real_escape_string( password_hash($_POST["pass"], PASSWORD_DEFAULT) ) );
+
+		$result = mysql_query( $query );
+
+		if (!$result) {
+		    echo "DB Error, could not query the database\n";
+		    echo 'MySQL Error: ' . mysql_error();
+		    exit;
+		}
+	}
+	else
+		return false;
 
 	$database -> close(); 
 ?>
