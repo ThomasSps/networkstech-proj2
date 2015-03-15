feff642215aea6eb48387e010b13e1cdaadd2465
Thomas Henry Spiteri <thomassps@gmail.com>
2015-03-15 03:22:19 +0200

    Created index php and css file

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Asanz - Posts</title>
+  <link rel="stylesheet" href="css/index.css">
+</head>
+<html>
+	<nav class="dropdownmenu">
+	  <ul>
+	    <li><a href="">+Post</a></li>
+	    <li><a href="">Dashboard</a></li>
+	    
+	    <!--
+	    <li><a href="">Articles on HTML5 & CSS3</a>
+	      <ul id="submenu">
+	        <li><a href="">Submenu item</a></li>
+	      </ul>
+	    </li>
+		-->
+
+	    <li><a href="">Logout</a></li>
+	  </ul>
+	</nav>
+	<body>
+		<article>
+			
+		</article>
+		<aside>
+			
+		</aside>
+	</body>
+</html>
